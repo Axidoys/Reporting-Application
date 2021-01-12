0c86c88e13931b76950d8d6f839a20f9b9e7b64e
Axel CHEMIN <axel.chemin@polytech-lille.net>
2021-01-12 10:10:22 +0100

    add: beginning of interface with logging system
    update .gitignore
    update README.md
    Main.java : [WIP] indexing of events per day

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # Reporting-Application
+
+
+This is the solution about the scope statement [here](http://dev.axel-chemin.fr/reporting-application/dl/MaintenanceReportTestReqDocV0_1.pdf)
+
+**Note** : links are available up to the 19th January 2021
+
+## How to use
+
+### Production simulation
+
+In order to test the program, you will need to use [this](http://dev.axel-chemin.fr/reporting-application/dl/machine-event-simulator-master.zip) simulator. A step-by-step guide is present in the README.
+
+Maybe you will have problems with obsolete dependencies, there is a [compiled](http://dev.axel-chemin.fr/reporting-application/dl/machine-event-simulator-compiled.zip) version with openjdk15.
+
+Once the server is listening to `61616` 
+
+### Main program
+
+You can use this program with ...
+
+
+
+
+
+
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,81 @@
 package com.company;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+
+        if(args.length>0) {
+
+            //Index raw log files
+            if (args[0].equals("index")) {
+                indexing(args);
+            }
+        }else{
+            System.out.println("No argument");
+        }
+
     }
+
+    public static void indexing(String[] args){
+        //args should contain : Workspace path ; Raw log file (which can be realtime input) //TODO realtime input
+        if(args.length==3){
+
+            // Verify existence of workspace
+            Path wsPath;
+            wsPath = Paths.get(args[1]); //TODO check syntax try and catch
+
+            if ( ! Files.isDirectory(wsPath) ) {
+                System.out.println("Workspace directory does not exist");
+                java.lang.System.exit(1);
+            }
+            // Verify existence of log file?
+            Path logPath ;
+            logPath = Paths.get(args[2]);
+
+            if ( ! Files.exists(logPath) ) {
+                System.out.println("Log file does not exist");
+                java.lang.System.exit(1);
+            }
+            // List files, clean old files (don't touch to alien-files), store 90 last days file paths
+            java.io.File wsFile = new File(args[1]);
+            FilenameFilter filter = new FilenameFilter() {
+                @Override
+                public boolean accept(File f, String name) {
+                    return name.toUpperCase().endsWith(".TXT");//TODO check isFile() (does not work)
+                }
+            };
+            File[] files = wsFile.listFiles(filter);
+
+            for(File file : files){
+                System.out.println(file.getName() + file.isFile());
+            }
+
+           //String[] daylyFiles;//TODO Array for the 90 last days
+            //TODO Remove old days
+
+
+            // Read the content from file
+            try(BufferedReader bufferedReader = new BufferedReader(new FileReader(args[0]))) {
+                String line = bufferedReader.readLine();
+                while(line != null) {
+                    System.out.println(line);
+                    line = bufferedReader.readLine();
+                }
+            } catch (IOException e) {
+                System.out.println("Error during the read of the log file");
+                java.lang.System.exit(1);
+            }
+
+        }
+        else{
+            System.out.println("index <Workspace path> <Log file>");
+        }
+    }
+
 }
